2628f8dcb7f12e8a204bb6c2baa31ebbcb9a0674
Randomcoder552 <67993317+Randomcoder552@users.noreply.github.com>
2021-11-27 08:52:04 -0500

    Add files via upload

---
+++ Project-Versions/IncreaseReaperDamage/IncreaseReaperCyclopsDamage.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using Logger = QModManager.Utility.Logger;
+
+namespace IncreaseReaperDamage
+{
+	class IncreaseReaperCyclopsDamage
+	{
+
+		[HarmonyPatch(typeof(MeleeAttack))]
+		[HarmonyPatch("ManagedUpdate")]
+
+		internal class Patch_MeleeAttack_ManagedUpdate
+		{
+			[HarmonyPostfix]
+			public static void Postfix(MeleeAttack __instance)
+			{
+
+				if (__instance.GetType() == typeof(ReaperMeleeAttack))
+				{
+					ReaperMeleeAttack reaperAttack = __instance as ReaperMeleeAttack;
+					float defaultCyclopsDamage = 160f;
+					float damageMultiplier = MainPatcher.config.cyclopsDamageMultiplier;
+
+					float oldReaperCyclopsDamage = reaperAttack.cyclopsDamage;
+					float newReaperCyclopsDamage = defaultCyclopsDamage * damageMultiplier;
+
+					reaperAttack.cyclopsDamage = newReaperCyclopsDamage;
+				}
+
+			}
+		}
+
+	}
+}
