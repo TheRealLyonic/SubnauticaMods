ec4731c2d45ffe2bba6d3d48df5aa0b38021654e
TheRealLyonic <67993317+TheRealLyonic@users.noreply.github.com>
2021-12-18 19:06:04 -0500

    Add files via upload

---
+++ Project-Versions (Most-Recent)/ImprovedReaper/IncreaseReaperStats.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using Logger = QModManager.Utility.Logger;
+
+namespace ImprovedReaper
+{
+	public static class IncreaseReaperStats
+	{
+		[HarmonyPatch(typeof(ReaperLeviathan))]
+		[HarmonyPatch("Update")]
+
+		internal class Patch_Reaper_Leviathan_Update
+		{
+			[HarmonyPostfix]
+			public static void Postfix(ReaperLeviathan __instance)
+			{
+				ReaperLeviathan reaper = __instance;
+
+				reaper.SetScale(MainPatcher.config.reaperSizeMultiplier);
+
+				reaper.hearingSensitivity = MainPatcher.config.reaperHSMultiplier;
+			}
+		}
+	}
+}
